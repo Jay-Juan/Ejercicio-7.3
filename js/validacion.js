ee9ebc927a84676c53bc12f96f8c99c47ee2adb7
Juan Pablo <jpcorvo219@gmail.com>
2023-08-09 14:50:54 -0300

    Update validacion.js

--- js/validacion.js
+++ js/validacion.js
@@ -10,6 +10,5 @@ function showAlertError() {
 function comparePassword() {
     let password1 = document.getElementById("password1")
     let password2 = document.getElementById("password2")
-    if(password1 === password2) {
-    }
+    return password1 === password2 
 }
