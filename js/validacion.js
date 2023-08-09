64f0660f9e18e29c64af74ceab4069a0bd02d78c
Juan Pablo <jpcorvo219@gmail.com>
2023-08-09 14:47:06 -0300

    compare password

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,11 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+
+function comparePassword() {
+    let password1 = document.getElementById("password1")
+    let password2 = document.getElementById("password2")
+    if(password1 === password2) {
+    }
+}
